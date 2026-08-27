189f664d693d31a9f7fa0ddd880a8ae4eed2272d
Xavier Wilson <xwilson@charlotte.edu>
2026-08-26 20:05:14 -0400

    Exercise 2 Complete

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,27 @@
+const DNA_TO_RNA: Record<string,string> = {
+    A: 'U',
+    T: 'A',
+    C: 'G',
+    G: 'C',
+}
+
+class NucleotideError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'NucelotideError'
+  }
+}
+
 export function transcribeDNA(dna: string): string {
-  return 'Dubba Wubba';
+  let result = '';
+
+  for (const char of dna) {
+    if (!(char in DNA_TO_RNA)) {
+      throw new NucleotideError('Invalid nucleotide provided.');
+    } else {
+      result += DNA_TO_RNA[char];
+    }
+
+  }
+  return result;
 }
