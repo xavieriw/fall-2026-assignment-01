8806649eb0a95190cd93964cf49b7ba01c365948
Xavier Wilson <xwilson@charlotte.edu>
2026-09-01 08:59:28 -0400

    fix exercise 3git add exercise03.ts git add exercise03.ts

--- src/exercise03.ts
+++ src/exercise03.ts
@@ -1,4 +1,4 @@
-export function getInventoryValue(
+export function getInventoryValue (
   inventory: Array<[string, number, number]>,
 ): number {
   return inventory.reduce((total,[,quantity,pricePerUnit]) => total + quantity * pricePerUnit, 0);
