c2122dfbc2da07d344cf94e7e317558e74da1f31
Xavier Wilson <xiwilson05@gmail.com>
2026-09-02 18:04:04 -0400

    exercise14 completed

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,23 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
-export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+export async function fetchPostBatch(
+  postIds: number[],
+): Promise<PostItem[]> {
+  const fetchPromises = postIds.map(async (id) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}`,
+    );
+    const data: PostItem = await response.json();
+    return {
+      id: data.id,
+      title: data.title,
+      body: data.body,
+    };
+  });
+
+  return Promise.all(fetchPromises);
 }
