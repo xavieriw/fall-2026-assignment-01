610c963c5b0701364874f6a50633fa8b038fd2b8
Xavier Wilson <xiwilson05@gmail.com>
2026-09-02 17:52:57 -0400

    exercise12 complete

--- src/exercise12.ts
+++ src/exercise12.ts
@@ -1,3 +1,12 @@
+export type RemoteUser = {
+  id: number;
+  name: string;
+  email: string;
+};
+
 export async function fetchUserEmails(): Promise<string[]> {
-  return [];
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: RemoteUser[] = await response.json();
+
+  return users.map((user) => user.email);
 }
