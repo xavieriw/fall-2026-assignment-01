618c8ff0f3ab9f5a2bd43cf5511797b16c294a59
Xavier Wilson <xiwilson05@gmail.com>
2026-09-02 17:02:02 -0400

    exercise08 completed

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,26 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  private listeners: {
+    [K in keyof T]?: Array<(data: T[K]) => void>;
+  } = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+    this.listeners[eventName]!.push(callback);
+  }
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    const callbacks = this.listeners[eventName];
+    if (callbacks) {
+      for (const callback of callbacks) {
+        callback(data);
+      }
+    }
+  }
 }
