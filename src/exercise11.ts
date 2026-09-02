155c17d0823d8f8e5394b088bf825acad30f72b6
Xavier Wilson <xiwilson05@gmail.com>
2026-09-02 17:45:49 -0400

    exercise11 completed

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,11 @@
+import * as fs from 'fs/promises';
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const timestamp = new Date().toISOString();
+  const logEntry = `${statusMessage} ${timestamp}\n`;
+
+  await fs.appendFile(filePath, logEntry, 'utf-8');
+}
