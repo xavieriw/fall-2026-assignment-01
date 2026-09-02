b67a2a81c837bf85d95d2770450b86aee6ec17b0
Xavier Wilson <xiwilson05@gmail.com>
2026-09-02 18:00:46 -0400

    finished exercise13

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,25 @@
-export type TodoItem = {};
+export type TodoItem = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
-export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+export async function fetchTodoSafe(
+  todoId: number,
+): Promise<TodoItem | null> {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    );
+
+    if (response.status !== 200) {
+      return null;
+    }
+
+    const data: TodoItem = await response.json();
+    return data;
+  } catch {
+    return null;
+  }
 }
