d0a5bd573b321d08baeb2459cf667ff62aab73f9
Xavier Wilson <xiwilson05@gmail.com>
2026-09-02 16:42:06 -0400

    finished exercise05

--- src/exercise05.ts
+++ src/exercise05.ts
@@ -1,9 +1,24 @@
-export type NetworkConfig = {};
+export type NetworkConfig = {
+  serverUrl: string;
+  port: number;
+};
 
-export type EnvironmentConfig = {};
+export type EnvironmentConfig = {
+  environment: 'dev' | 'prod';
+  timeout: number;
+};
 
 export type AppConfig = NetworkConfig & EnvironmentConfig;
 
 export function initializeConfig(userOverrides: Partial<AppConfig>): AppConfig {
-  return {};
+  const defaultConfig: AppConfig = {
+    serverUrl: "http://localhost",
+    port: 8080,
+    environment: "dev",
+    timeout: 3000,
+  }
+  return {
+    ...defaultConfig,
+    ...userOverrides,
+  };
 }
